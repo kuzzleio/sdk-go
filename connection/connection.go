6aedbbeb09d751a0d201d630f8d275110e334304
Anthony Sendra <asendra@kaliop.com>
2017-07-11 17:00:30 +0200

    gitignore

---
+++ connection/connection.go
@@ -0,0 +1,9 @@
+package connection
+
+import "github.com/kuzzleio/sdk-go/types"
+
+type Connection interface {
+	Connect() (bool, error)
+	Send([]byte, *types.Options, chan<- types.KuzzleResponse, string) error
+	Close() error
+}
