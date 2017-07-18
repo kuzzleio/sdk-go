e12072de38b00232439bac44cee061e2bab555d7
Anthony Sendra <asendra@kaliop.com>
2017-07-18 18:04:31 +0200

    implement unit test option createDocument

--- collection/create_document_test.go
+++ collection/create_document_test.go
@@ -8,6 +8,7 @@ import (
   "encoding/json"
   "github.com/kuzzleio/sdk-go/types"
   "github.com/kuzzleio/sdk-go/collection"
+  "fmt"
 )
 
 func TestCreateDocumentError(t *testing.T) {
@@ -24,6 +25,22 @@ func TestCreateDocumentError(t *testing.T) {
 
   _, err := collection.NewCollection(k, "collection", "index").CreateDocument("id", Document{Title: "yolo"}, nil)
   assert.NotNil(t, err)
+}
+
+func TestCreateDocumentWrongOptionError(t *testing.T) {
+  type Document struct {
+    Title string
+  }
+
+  c := &internal.MockedConnection{
+    MockSend: func(query []byte, options *types.Options) types.KuzzleResponse {
+      return types.KuzzleResponse{}
+    },
+  }
+  k, _ := kuzzle.NewKuzzle(c, nil)
+  newCollection := collection.NewCollection(k, "collection", "index")
+  _, err := newCollection.CreateDocument("id", Document{Title: "yolo"}, &types.Options{IfExist: "unknown"})
+  assert.Equal(t, "Invalid value for the 'ifExist' option: 'unknown'", fmt.Sprint(err))
 }
 
 func TestCreateDocument(t *testing.T) {
@@ -55,4 +72,50 @@ func TestCreateDocument(t *testing.T) {
 
   res, _ := collection.NewCollection(k, "collection", "index").CreateDocument(id, Document{Title: "yolo"}, nil)
   assert.Equal(t, id, res.Id)
+}
+
+func TestCreateDocumentReplace(t *testing.T) {
+  type Document struct {
+    Title string
+  }
+
+  c := &internal.MockedConnection{
+    MockSend: func(query []byte, options *types.Options) types.KuzzleResponse {
+      parsedQuery := &types.KuzzleRequest{}
+      json.Unmarshal(query, parsedQuery)
+
+      assert.Equal(t, "createOrReplace", parsedQuery.Action)
+
+      res := types.Document{Id: "id", Source: []byte(`{"title": "yolo"}`)}
+      r, _ := json.Marshal(res)
+      return types.KuzzleResponse{Result: r}
+    },
+  }
+  k, _ := kuzzle.NewKuzzle(c, nil)
+
+  newCollection := collection.NewCollection(k, "collection", "index")
+  newCollection.CreateDocument("id", Document{Title: "yolo"}, &types.Options{IfExist: "replace"})
+}
+
+func TestCreateDocumentCreate(t *testing.T) {
+  type Document struct {
+    Title string
+  }
+
+  c := &internal.MockedConnection{
+    MockSend: func(query []byte, options *types.Options) types.KuzzleResponse {
+      parsedQuery := &types.KuzzleRequest{}
+      json.Unmarshal(query, parsedQuery)
+
+      assert.Equal(t, "create", parsedQuery.Action)
+
+      res := types.Document{Id: "id", Source: []byte(`{"title": "yolo"}`)}
+      r, _ := json.Marshal(res)
+      return types.KuzzleResponse{Result: r}
+    },
+  }
+  k, _ := kuzzle.NewKuzzle(c, nil)
+
+  newCollection := collection.NewCollection(k, "collection", "index")
+  newCollection.CreateDocument("id", Document{Title: "yolo"}, &types.Options{IfExist: "error"})
 }
