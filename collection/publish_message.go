00299fe7654afe0f877800356ed687e27837c30a
Samuel Bouic <samuel.bouic@gmail.com>
2017-07-20 15:02:52 +0200

    Fixed indentation

--- collection/publish_message.go
+++ collection/publish_message.go
@@ -1,9 +1,9 @@
 package collection
 
 import (
-	"github.com/kuzzleio/sdk-go/types"
-	"errors"
-	"encoding/json"
+  "github.com/kuzzleio/sdk-go/types"
+  "errors"
+  "encoding/json"
 )
 
 /*
@@ -14,25 +14,25 @@ import (
       Additional information passed to notifications to other users
 */
 func (dc Collection) PublishMessage(document interface{}, options *types.Options) (*types.RealtimeResponse, error) {
-	ch := make(chan types.KuzzleResponse)
+  ch := make(chan types.KuzzleResponse)
 
-	query := types.KuzzleRequest{
-		Collection: dc.collection,
-		Index:      dc.index,
-		Controller: "realtime",
-		Action:     "publish",
-		Body:       document,
-	}
-	go dc.kuzzle.Query(query, options, ch)
+  query := types.KuzzleRequest{
+    Collection: dc.collection,
+    Index:      dc.index,
+    Controller: "realtime",
+    Action:     "publish",
+    Body:       document,
+  }
+  go dc.kuzzle.Query(query, options, ch)
 
-	res := <-ch
+  res := <-ch
 
-	if res.Error.Message != "" {
-		return nil, errors.New(res.Error.Message)
-	}
+  if res.Error.Message != "" {
+    return nil, errors.New(res.Error.Message)
+  }
 
-	response := &types.RealtimeResponse{}
-	json.Unmarshal(res.Result, response)
+  response := &types.RealtimeResponse{}
+  json.Unmarshal(res.Result, response)
 
-	return response, nil
+  return response, nil
 }
