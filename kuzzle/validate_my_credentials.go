c6f8a4e6d1265ff86f9bf6468d2a96c81079f527
jenow <jenova.ke20@gmail.com>
2017-07-25 15:19:45 +0200

    Implements ValidateMyCredentials

---
+++ kuzzle/validate_my_credentials.go
@@ -0,0 +1,34 @@
+package kuzzle
+
+import (
+  "github.com/kuzzleio/sdk-go/types"
+  "errors"
+  "encoding/json"
+)
+
+/*
+ * Get credential information of the specified strategy for the current user.
+ */
+func (k Kuzzle) ValidateMyCredentials(strategy string, credentials interface{}, options *types.Options) (bool, error) {
+  result := make(chan types.KuzzleResponse)
+
+  query := types.KuzzleRequest{
+    Controller: "auth",
+    Action:     "validateMyCredentials",
+    Strategy:   strategy,
+    Body:       credentials,
+  }
+
+  go k.Query(query, nil, result)
+
+  res := <-result
+
+  if res.Error.Message != "" {
+    return false, errors.New(res.Error.Message)
+  }
+
+  var r bool
+  json.Unmarshal(res.Result, &r)
+
+  return r, nil
+}
