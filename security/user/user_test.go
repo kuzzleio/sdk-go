df5e29a9f2a8b2f32ff95615875e285cef2e60a8
Samuel Bouic <samuel.bouic@gmail.com>
2017-07-26 17:22:44 +0200

    Add IsActionAllowed route in Security

--- security/user/user_test.go
+++ security/user/user_test.go
@@ -109,7 +109,7 @@ func TestGetRights(t *testing.T) {
 				UserRights []types.UserRights `json:"hits"`
 			}
 			userRights := []types.UserRights{}
-			userRights = append(userRights, types.UserRights{Controller:"wow-controll", Action: "such-action", Index: "much indexes", Collection: "very collection", Value: "wow"})
+			userRights = append(userRights, types.UserRights{Controller:"wow-controller", Action: "such-action", Index: "much-index", Collection: "very-collection", Value: "wow"})
 			actualRights := resultUserRights{UserRights: userRights}
 			r, _ := json.Marshal(actualRights)
 			return types.KuzzleResponse{Result: r}
@@ -120,7 +120,69 @@ func TestGetRights(t *testing.T) {
 	res, _ := security.NewSecurity(k).User.GetRights(id, nil)
 
 	expectedRights := []types.UserRights{}
-	expectedRights = append(expectedRights, types.UserRights{Controller:"wow-controll", Action: "such-action", Index: "much indexes", Collection: "very collection", Value: "wow"})
+	expectedRights = append(expectedRights, types.UserRights{Controller:"wow-controller", Action: "such-action", Index: "much-index", Collection: "very-collection", Value: "wow"})
 
 	assert.Equal(t, expectedRights, res)
+}
+
+func TestIsActionAllowedNilRights(t *testing.T) {
+	k, _ := kuzzle.NewKuzzle(&internal.MockedConnection{}, nil)
+
+	_, err := security.NewSecurity(k).User.IsActionAllowed(nil, "wow-controller", "such-action", "", "")
+	assert.NotNil(t, err)
+}
+
+func TestIsActionAllowedEmptyController(t *testing.T) {
+	k, _ := kuzzle.NewKuzzle(&internal.MockedConnection{}, nil)
+
+	_, err := security.NewSecurity(k).User.IsActionAllowed([]types.UserRights{}, "", "such-action", "", "")
+	assert.NotNil(t, err)
+}
+
+func TestIsActionAllowedEmptyAction(t *testing.T) {
+	k, _ := kuzzle.NewKuzzle(&internal.MockedConnection{}, nil)
+
+	_, err := security.NewSecurity(k).User.IsActionAllowed([]types.UserRights{}, "wow-controller", "", "", "")
+	assert.NotNil(t, err)
+}
+
+func TestIsActionAllowedEmptyRights(t *testing.T) {
+	k, _ := kuzzle.NewKuzzle(&internal.MockedConnection{}, nil)
+
+	res, _ := security.NewSecurity(k).User.IsActionAllowed([]types.UserRights{}, "wow-controller", "such-action", "much-index", "very-collection")
+
+	assert.Equal(t, "denied", res)
+}
+
+func TestIsActionAllowedResultAllowed(t *testing.T) {
+	k, _ := kuzzle.NewKuzzle(&internal.MockedConnection{}, nil)
+
+	userRights := []types.UserRights{}
+	userRights = append(userRights, types.UserRights{Controller:"wow-controller", Action: "*", Index: "much-index", Collection: "very-collection", Value: "allowed"})
+
+	res, _ := security.NewSecurity(k).User.IsActionAllowed(userRights, "wow-controller", "such-action", "much-index", "very-collection")
+
+	assert.Equal(t, "allowed", res)
+}
+
+func TestIsActionAllowedResultConditional(t *testing.T) {
+	k, _ := kuzzle.NewKuzzle(&internal.MockedConnection{}, nil)
+
+	userRights := []types.UserRights{}
+	userRights = append(userRights, types.UserRights{Controller:"wow-controller", Action: "*", Index: "much-index", Collection: "very-collection", Value: "conditional"})
+
+	res, _ := security.NewSecurity(k).User.IsActionAllowed(userRights, "wow-controller", "action", "much-index", "very-collection")
+
+	assert.Equal(t, "conditional", res)
+}
+
+func TestIsActionAllowedResultDenied(t *testing.T) {
+	k, _ := kuzzle.NewKuzzle(&internal.MockedConnection{}, nil)
+
+	userRights := []types.UserRights{}
+	userRights = append(userRights, types.UserRights{Controller:"wow-controller.", Action: "action-such", Index: "much-index", Collection: "very-collection", Value: "allowed"})
+
+	res, _ := security.NewSecurity(k).User.IsActionAllowed(userRights, "wow-controller", "action", "much-index", "very-collection")
+
+	assert.Equal(t, "denied", res)
 }
