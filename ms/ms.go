b5462acea2a2adf06d7770e8535d7a909043db61
David Bengsch <dbengsch@kaliop.com>
2017-07-31 10:51:23 +0200

    Add package headers to all ms files

--- ms/ms.go
+++ ms/ms.go
@@ -1,13 +1,2 @@
 package ms
 
-import "github.com/kuzzleio/sdk-go/kuzzle"
-
-type Ms struct {
-	Kuzzle *kuzzle.Kuzzle
-}
-
-func NewMs(kuzzle *kuzzle.Kuzzle) *Ms {
-	return &Ms{
-		Kuzzle: kuzzle,
-	}
-}
---
+++ ms/sismember_test.go
@@ -0,0 +1,2 @@
+package ms_test
+
