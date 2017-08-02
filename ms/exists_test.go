b6894602e8d7b8705ae5c5fb57234e7bbc044e28
David Bengsch <dbengsch@kaliop.com>
2017-08-02 11:08:27 +0200

    Increase coverage

--- ms/exists_test.go
+++ ms/exists_test.go
@@ -1,1 +1,50 @@
 package ms_test
+
+import (
+	"encoding/json"
+	"github.com/kuzzleio/sdk-go/internal"
+	"github.com/kuzzleio/sdk-go/kuzzle"
+	"github.com/kuzzleio/sdk-go/types"
+	"github.com/stretchr/testify/assert"
+	MemoryStorage "github.com/kuzzleio/sdk-go/ms"
+	"testing"
+)
+
+func TestExistsError(t *testing.T) {
+	c := &internal.MockedConnection{
+		MockSend: func(query []byte, options types.QueryOptions) types.KuzzleResponse {
+			return types.KuzzleResponse{Error: types.MessageError{Message: "Unit test error"}}
+		},
+	}
+	k, _ := kuzzle.NewKuzzle(c, nil)
+	memoryStorage := MemoryStorage.NewMs(k)
+	qo := types.NewQueryOptions()
+
+	_, err := memoryStorage.Exists([]string{}, qo)
+
+	assert.NotNil(t, err)
+}
+
+func TestExists(t *testing.T) {
+	c := &internal.MockedConnection{
+		MockSend: func(query []byte, options types.QueryOptions) types.KuzzleResponse {
+			parsedQuery := &types.KuzzleRequest{}
+			json.Unmarshal(query, parsedQuery)
+
+			assert.Equal(t, "ms", parsedQuery.Controller)
+			assert.Equal(t, "exists", parsedQuery.Action)
+			assert.Equal(t, "some", parsedQuery.Body.(map[string]interface {})["keys"].([]interface{})[0].(string))
+			assert.Equal(t, "keys", parsedQuery.Body.(map[string]interface {})["keys"].([]interface{})[1].(string))
+
+			r, _ := json.Marshal(1)
+			return types.KuzzleResponse{Result: r}
+		},
+	}
+	k, _ := kuzzle.NewKuzzle(c, nil)
+	memoryStorage := MemoryStorage.NewMs(k)
+	qo := types.NewQueryOptions()
+
+	res, _ := memoryStorage.Exists([]string{"some", "keys"}, qo)
+
+	assert.Equal(t, 1, res)
+}
--- ms/geodist_test.go
+++ ms/geodist_test.go
@@ -1,1 +1,63 @@
 package ms_test
+
+import (
+	"encoding/json"
+	"github.com/kuzzleio/sdk-go/internal"
+	"github.com/kuzzleio/sdk-go/kuzzle"
+	"github.com/kuzzleio/sdk-go/types"
+	"github.com/stretchr/testify/assert"
+	MemoryStorage "github.com/kuzzleio/sdk-go/ms"
+	"testing"
+	"fmt"
+)
+
+func TestGeodistEmptyKey(t *testing.T) {
+	k, _ := kuzzle.NewKuzzle(&internal.MockedConnection{}, nil)
+	memoryStorage := MemoryStorage.NewMs(k)
+	qo := types.NewQueryOptions()
+
+	_, err := memoryStorage.Geodist("", "bar", "barbar", qo)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "Ms.Geodist: key required", fmt.Sprint(err))
+}
+
+func TestGeodistError(t *testing.T) {
+	c := &internal.MockedConnection{
+		MockSend: func(query []byte, options types.QueryOptions) types.KuzzleResponse {
+			return types.KuzzleResponse{Error: types.MessageError{Message: "Unit test error"}}
+		},
+	}
+	k, _ := kuzzle.NewKuzzle(c, nil)
+	memoryStorage := MemoryStorage.NewMs(k)
+	qo := types.NewQueryOptions()
+
+	_, err := memoryStorage.Geodist("", "bar", "barbar", qo)
+
+	assert.NotNil(t, err)
+}
+
+func TestGeodist(t *testing.T) {
+	c := &internal.MockedConnection{
+		MockSend: func(query []byte, options types.QueryOptions) types.KuzzleResponse {
+			parsedQuery := &types.KuzzleRequest{}
+			json.Unmarshal(query, parsedQuery)
+
+			assert.Equal(t, "ms", parsedQuery.Controller)
+			assert.Equal(t, "geodist", parsedQuery.Action)
+			assert.Equal(t, "foo", parsedQuery.Id)
+			assert.Equal(t, "bar", parsedQuery.Member1)
+			assert.Equal(t, "barbar", parsedQuery.Member2)
+
+			r, _ := json.Marshal(float64(42))
+			return types.KuzzleResponse{Result: r}
+		},
+	}
+	k, _ := kuzzle.NewKuzzle(c, nil)
+	memoryStorage := MemoryStorage.NewMs(k)
+	qo := types.NewQueryOptions()
+
+	res, _ := memoryStorage.Geodist("foo", "bar", "barbar", qo)
+
+	assert.Equal(t, float64(42), res)
+}
