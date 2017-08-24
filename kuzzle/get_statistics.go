b66c954641a6d80e2e0f91987583cb10abe86fe2
jenow <jenova.ke20@gmail.com>
2017-08-24 10:20:14 +0200

    added timestamp to getstats

--- kuzzle/get_statistics.go
+++ kuzzle/get_statistics.go
@@ -4,17 +4,30 @@ import (
 	"encoding/json"
 	"errors"
 	"github.com/kuzzleio/sdk-go/types"
+	"time"
 )
 
 /*
  * Get Kuzzle usage statistics
  */
-func (k Kuzzle) GetStatistics(options types.QueryOptions) (types.Statistics, error) {
+func (k Kuzzle) GetStatistics(timestamp *time.Time, options types.QueryOptions) (types.Statistics, error) {
 	result := make(chan types.KuzzleResponse)
+
+	type data struct {
+		since string `json:"since"`
+	}
+
+	var d data
+	if timestamp != nil {
+		d = data{
+			timestamp.String(),
+		}
+	}
 
 	query := types.KuzzleRequest{
 		Controller: "server",
 		Action:     "getLastStats",
+		Body: d,
 	}
 
 	go k.Query(query, options, result)
