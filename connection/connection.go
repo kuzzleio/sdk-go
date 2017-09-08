0f7066aadf46eb5380e2bb6c16a197011fa58f27
jenow <jenova.ke20@gmail.com>
2017-09-08 16:36:23 +0200

    add RemoveAllListener to the Kuzzle struct

--- connection/connection.go
+++ connection/connection.go
@@ -7,6 +7,7 @@ import (
 
 type Connection interface {
 	AddListener(event int, channel chan<- interface{})
+	RemoveAllListeners(event int)
 	Connect() (bool, error)
 	Send([]byte, types.QueryOptions, chan<- types.KuzzleResponse, string) error
 	Close() error
