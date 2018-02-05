39082748005853fe00753d45bc1c3d01fe98e8de
jenow <jenova.ke20@gmail.com>
2018-02-05 18:10:49 +0100

    coverage

--- collection/room_subscribe_test.go
+++ collection/room_subscribe_test.go
@@ -66,6 +66,32 @@ func TestRoomSubscribe(t *testing.T) {
 	NewRoom(NewCollection(k, "collection", "index"), nil, nil).Subscribe(nil)
 }
 
+func TestRoomSubscribeAlreadyActive(t *testing.T) {
+	var k *kuzzle.Kuzzle
+
+	c := &internal.MockedConnection{
+		MockSend: func(query []byte, options types.QueryOptions) *types.KuzzleResponse {
+			roomRaw := []byte(`{"requestId": "rqid", "channel": "foo", "roomId": "42"}`)
+			return &types.KuzzleResponse{Result: roomRaw}
+		},
+	}
+	k, _ = kuzzle.NewKuzzle(c, nil)
+	c.SetState(state.Connected)
+
+	r := NewRoom(NewCollection(k, "collection", "index"), nil, nil)
+	r.internalState = active
+	r.subscribeResponseChan = make(chan *types.SubscribeResponse)
+	done := make(chan bool)
+
+	go func() {
+		<-r.subscribeResponseChan
+		done <- true
+	}()
+	r.Subscribe(nil)
+
+	<-done
+}
+
 func ExampleRoom_Subscribe() {
 	c := &internal.MockedConnection{}
 	k, _ := kuzzle.NewKuzzle(c, nil)
--- collection/room_test.go
+++ collection/room_test.go
@@ -1,10 +1,9 @@
-package collection_test
+package collection
 
 import (
 	"fmt"
 	"testing"
 
-	"github.com/kuzzleio/sdk-go/collection"
 	"github.com/kuzzleio/sdk-go/internal"
 	"github.com/kuzzleio/sdk-go/kuzzle"
 	"github.com/kuzzleio/sdk-go/state"
@@ -21,7 +20,7 @@ func TestInitRoomWithoutOptions(t *testing.T) {
 
 	k, _ := kuzzle.NewKuzzle(c, nil)
 
-	r := collection.NewRoom(collection.NewCollection(k, "collection", "index"), nil, nil)
+	r := NewRoom(NewCollection(k, "collection", "index"), nil, nil)
 
 	assert.NotNil(t, r)
 }
@@ -37,7 +36,7 @@ func TestRoomFilters(t *testing.T) {
 	}
 
 	k, _ = kuzzle.NewKuzzle(c, nil)
-	cl := collection.NewCollection(k, "collection", "index")
+	cl := NewCollection(k, "collection", "index")
 
 	type SubscribeFiltersValues struct {
 		Values []string `json:"values"`
@@ -61,7 +60,7 @@ func TestRoomFilters(t *testing.T) {
 func ExampleRoom_Filters() {
 	c := &internal.MockedConnection{}
 	k, _ := kuzzle.NewKuzzle(c, nil)
-	cl := collection.NewCollection(k, "collection", "index")
+	cl := NewCollection(k, "collection", "index")
 
 	type SubscribeFiltersValues struct {
 		Values []string `json:"values"`
@@ -93,7 +92,7 @@ func TestRoomRealtimeChannel(t *testing.T) {
 	}
 
 	k, _ = kuzzle.NewKuzzle(c, nil)
-	cl := collection.NewCollection(k, "collection", "index")
+	cl := NewCollection(k, "collection", "index")
 
 	type SubscribeFiltersValues struct {
 		Values []string `json:"values"`
@@ -117,7 +116,7 @@ func TestRoomRealtimeChannel(t *testing.T) {
 func ExampleRoom_RealtimeChannel() {
 	c := &internal.MockedConnection{}
 	k, _ := kuzzle.NewKuzzle(c, nil)
-	cl := collection.NewCollection(k, "collection", "index")
+	cl := NewCollection(k, "collection", "index")
 
 	type SubscribeFiltersValues struct {
 		Values []string `json:"values"`
@@ -137,4 +136,162 @@ func ExampleRoom_RealtimeChannel() {
 	rtChannel := res.Room.RealtimeChannel()
 
 	fmt.Println(rtChannel)
+}
+
+func TestAddListener(t *testing.T) {
+	called := false
+
+	c := &internal.MockedConnection{
+		MockAddListener: func(e int, c chan<- interface{}) {
+			called = true
+		},
+	}
+	k, _ := kuzzle.NewKuzzle(c, nil)
+	r := NewRoom(NewCollection(k, "collection", "index"), nil, nil)
+
+	ch := make(chan interface{})
+
+	r.AddListener(0, ch)
+	assert.Equal(t, true, called)
+}
+
+func TestRemoveListener(t *testing.T) {
+	called := false
+
+	c := &internal.MockedConnection{
+		MockRemoveListener: func(e int, c chan<- interface{}) {
+			called = true
+		},
+	}
+	k, _ := kuzzle.NewKuzzle(c, nil)
+	r := NewRoom(NewCollection(k, "collection", "index"), nil, nil)
+
+	ch := make(chan interface{})
+
+	r.RemoveListener(0, ch)
+	assert.Equal(t, true, called)
+}
+func TestRemoveAllListener(t *testing.T) {
+	called := false
+
+	c := &internal.MockedConnection{
+		MockRemoveAllListeners: func(e int) {
+			called = true
+		},
+	}
+	k, _ := kuzzle.NewKuzzle(c, nil)
+	r := NewRoom(NewCollection(k, "collection", "index"), nil, nil)
+
+	r.RemoveAllListeners(0)
+	assert.Equal(t, true, called)
+}
+
+func TestOnce(t *testing.T) {
+	called := false
+
+	c := &internal.MockedConnection{
+		MockOnce: func(e int, c chan<- interface{}) {
+			called = true
+		},
+	}
+	k, _ := kuzzle.NewKuzzle(c, nil)
+	r := NewRoom(NewCollection(k, "collection", "index"), nil, nil)
+
+	ch := make(chan interface{})
+
+	r.Once(0, ch)
+	assert.Equal(t, true, called)
+}
+
+func TestOn(t *testing.T) {
+	called := false
+
+	c := &internal.MockedConnection{
+		MockAddListener: func(e int, c chan<- interface{}) {
+			called = true
+		},
+	}
+	k, _ := kuzzle.NewKuzzle(c, nil)
+	r := NewRoom(NewCollection(k, "collection", "index"), nil, nil)
+
+	ch := make(chan interface{})
+
+	r.On(0, ch)
+	assert.Equal(t, true, called)
+}
+
+func TestListenerCount(t *testing.T) {
+	called := false
+
+	c := &internal.MockedConnection{
+		MockListenerCount: func(e int) int {
+			called = true
+			return -1
+		},
+	}
+	k, _ := kuzzle.NewKuzzle(c, nil)
+	r := NewRoom(NewCollection(k, "collection", "index"), nil, nil)
+
+	r.ListenerCount(0)
+	assert.Equal(t, true, called)
+}
+func TestOnDoneError(t *testing.T) {
+	c := &internal.MockedConnection{
+		MockSend: func(query []byte, options types.QueryOptions) *types.KuzzleResponse {
+			return nil
+		},
+	}
+
+	k, _ := kuzzle.NewKuzzle(c, nil)
+	r := NewRoom(NewCollection(k, "collection", "index"), nil, nil)
+	ch := make(chan *types.SubscribeResponse)
+	done := make(chan bool)
+
+	go func() {
+		<-ch
+		done <- true
+	}()
+	r.err = &types.KuzzleError{Message: "Room has an error"}
+	r.OnDone(ch)
+
+	<-done
+}
+
+func TestOnDoneAlreadyActive(t *testing.T) {
+	c := &internal.MockedConnection{
+		MockSend: func(query []byte, options types.QueryOptions) *types.KuzzleResponse {
+			return nil
+		},
+	}
+
+	k, _ := kuzzle.NewKuzzle(c, nil)
+	r := NewRoom(NewCollection(k, "collection", "index"), nil, nil)
+	ch := make(chan *types.SubscribeResponse)
+	done := make(chan bool)
+
+	go func() {
+		<-ch
+		done <- true
+	}()
+
+	r.internalState = active
+	r.OnDone(ch)
+
+	<-done
+}
+func TestOnDone(t *testing.T) {
+	c := &internal.MockedConnection{
+		MockSend: func(query []byte, options types.QueryOptions) *types.KuzzleResponse {
+			roomRaw := []byte(`{"requestId": "rqid", "channel": "foo", "roomId": "42"}`)
+			return &types.KuzzleResponse{Result: roomRaw}
+		},
+	}
+
+	k, _ := kuzzle.NewKuzzle(c, nil)
+	r := NewRoom(NewCollection(k, "collection", "index"), nil, nil)
+	ch := make(chan<- *types.SubscribeResponse)
+	r.OnDone(ch)
+
+	r.Subscribe(nil)
+	assert.Equal(t, ch, r.subscribeResponseChan)
 }
