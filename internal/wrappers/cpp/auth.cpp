99c876a695d84bdea9c05dcc995d35c77d672082
jenow <jenova.ke20@gmail.com>
2018-05-22 12:07:56 +0200

    fix return in getCurrentUser cpp

--- internal/wrappers/cpp/auth.cpp
+++ internal/wrappers/cpp/auth.cpp
@@ -64,7 +64,9 @@ namespace kuzzleio {
     user_result *r = kuzzle_get_current_user(_auth);
     if (r->error != NULL)
         throwExceptionFromStatus(r);
+    user *u = r->result;
     kuzzle_free_user_result(r);
+    return u;
   }
 
   std::string Auth::getMyCredentials(const std::string& strategy, query_options *options) Kuz_Throw_KuzzleException {
