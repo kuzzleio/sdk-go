78d454699165811c354d9c9cccc5a518b88261f5
jenow <jenova.ke20@gmail.com>
2018-06-14 16:07:47 +0200

    java e2e collection controller

---
+++ internal/wrappers/features/collection.feature
@@ -0,0 +1,60 @@
+Feature: Collection management
+
+  Scenario: Create a collection
+    Given Kuzzle Server is running
+    And there is an index 'test-index'
+    When I create a collection 'collection-test-collection'
+    Then the collection should exists
+
+  Scenario: Check if a collection exists
+    Given Kuzzle Server is running
+    And there is an index 'test-index'
+    And it has a collection 'test-collection'
+    When I check if the collection exists
+    Then it should exists
+
+  Scenario: List existing collections
+    Given Kuzzle Server is running
+    And there is an index 'list-test-index'
+    And it has a collection 'test-collection1'
+    And it has a collection 'test-collection2'
+    When I list the collections
+    Then I should have 2 collections
+
+  Scenario: Truncate a collection
+    Given Kuzzle Server is running
+    And there is an index 'test-index'
+    And it has a collection 'test-collection'
+    And the collection has a document with id 'my-document-id'
+    And the collection has a document with id 'my-document-id2'
+    When I truncate the collection
+    Then it should be empty
+
+  Scenario: Create a collection with a custom mapping
+    Given Kuzzle Server is running
+    And there is an index 'test-index'
+    And it has a collection 'test-collection'
+    When I update the mapping
+    Then the mapping should be updated
+
+  Scenario: Update specifications
+    Given Kuzzle Server is running
+    And there is an index 'test-index'
+    And it has a collection 'test-collection'
+    When I update the specifications
+    Then they should be updated
+
+  Scenario: Validate specifications
+    Given Kuzzle Server is running
+    And there is an index 'test-index'
+    And it has a collection 'test-collection'
+    When I validate the specifications
+    Then they should be validated
+
+  Scenario: Delete specifications
+    Given Kuzzle Server is running
+    And there is an index 'test-index'
+    And it has a collection 'test-collection'
+    And has specifications
+    When I delete the specifications
+    Then the specifications must not exist
---
+++ internal/wrappers/features/java/src/test/java/gradle/cucumber/Collectiondefs.java
@@ -0,0 +1,131 @@
+package gradle.cucumber;
+
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.kuzzle.sdk.Kuzzle;
+import io.kuzzle.sdk.NotFoundException;
+import io.kuzzle.sdk.QueryOptions;
+import io.kuzzle.sdk.SearchResult;
+import org.junit.Assert;
+
+import javax.management.remote.NotificationResult;
+
+public class Collectiondefs {
+    private Kuzzle k;
+    private World world;
+    private boolean exists = false;
+    String listCollections;
+    private boolean validateSpecs = false;
+
+    @Before
+    public void before() {
+        k = KuzzleSingleton.getInstance();
+    }
+
+    @When("^I create a collection \'([^\"]*)\'$")
+    public void i_create_a_collection_test_collection(String collection) throws Exception {
+        k.getCollection().create(world.index, collection);
+        world.collection = collection;
+    }
+
+    @Then("^the collection should exists$")
+    public void the_collection_should_exists() throws Exception {
+        Assert.assertTrue(k.getCollection().exists(world.index, world.collection));
+    }
+
+    @When("^I check if the collection exists$")
+    public void i_check_if_the_collection_exists() throws Exception {
+        exists = k.getCollection().exists(world.index, world.collection);
+    }
+
+    @Then("^it should exists$")
+    public void it_should_exists() throws Exception {
+        Assert.assertTrue(exists);
+    }
+
+    @When("^I list the collections$")
+    public void i_list_the_collections() throws Exception {
+        listCollections = k.getCollection().list(world.index);
+    }
+
+    @Then("^I should have 2 collections$")
+    public void i_should_have_collections() throws Exception {
+        Assert.assertNotNull(this.listCollections);
+        Assert.assertEquals("{\"type\":\"all\",\"collections\":[{\"name\":\"test-collection1\",\"type\":\"stored\"},{\"name\":\"test-collection2\",\"type\":\"stored\"}],\"from\":0,\"size\":10}", this.listCollections);
+    }
+
+    @When("^I truncate the collection$")
+    public void i_truncate_the_collection() throws Exception {
+        QueryOptions o = new QueryOptions();
+        o.setRefresh("wait_for");
+
+        k.getCollection().truncate(world.index, world.collection, o);
+    }
+
+    @Then("^it should be empty$")
+    public void it_should_be_empty() throws Exception {
+        SearchResult res = k.getDocument().search(world.index, world.collection, "{}");
+        Assert.assertEquals("[]", res.getDocuments());
+    }
+
+    @When("^I update the mapping$")
+    public void i_update_the_mapping() throws Exception {
+        k.getCollection().updateMapping(world.index, world.collection, "{" +
+                "\"properties\": {" +
+                "    \"foo\": {" +
+                "      \"type\": \"string\"" +
+                "    }" +
+                "}" +
+                "}");
+    }
+
+    @Then("^the mapping should be updated$")
+    public void the_mapping_should_be_updated() throws Exception {
+        String mapping = k.getCollection().getMapping(world.index, world.collection);
+        Assert.assertEquals("{\"test-index\":{\"mappings\":{\"test-collection\":{\"properties\":{\"foo\":{\"type\":\"text\",\"fields\":{\"keyword\":{\"type\":\"keyword\",\"ignore_above\":256}}}}}}}}", mapping);
+    }
+
+    @When("^I update the specifications$")
+    public void i_update_the_specifications() throws Exception {
+        k.getCollection().updateSpecifications(world.index, world.collection, "{\""+world.index+"\":{\""+world.collection+"\":{\"strict\":true}}}");
+    }
+
+    @Then("^they should be updated$")
+    public void they_should_be_updated() throws Exception {
+        Assert.assertEquals("{\"validation\":{\"strict\":true},\"index\":\"test-index\",\"collection\":\"test-collection\"}", k.getCollection().getSpecifications(world.index, world.collection));
+    }
+
+    @When("^I validate the specifications$")
+    public void i_validate_the_specifications() throws Exception {
+        this.validateSpecs = k.getCollection().validateSpecifications("{\""+world.index+"\":{\""+world.collection+"\":{\"strict\":true}}}");
+    }
+
+    @Then("^they should be validated$")
+    public void they_should_be_validated() throws Exception {
+        Assert.assertTrue(this.validateSpecs);
+    }
+
+    @Given("^has specifications$")
+    public void has_specifications() throws Exception {
+        k.getCollection().updateSpecifications(world.index, world.collection, "{\""+world.index+"\":{\""+world.collection+"\":{\"strict\":true}}}");
+    }
+
+    @When("^I delete the specifications$")
+    public void i_delete_the_specifications() throws Exception {
+        k.getCollection().deleteSpecifications(world.index, world.collection);
+    }
+
+    @Then("^the specifications must not exist$")
+    public void the_specifications_must_not_exist() throws Exception {
+        boolean notFound = false;
+        try {
+            k.getCollection().getSpecifications(world.index, world.collection);
+        } catch (NotFoundException e) {
+            notFound = true;
+        }
+        Assert.assertTrue(notFound);
+    }
+
+}
