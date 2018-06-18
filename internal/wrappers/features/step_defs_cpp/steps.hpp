d7a2962f25789af617703bfe0841d0d59fe27fff
jenow <jenova.ke20@gmail.com>
2018-06-18 16:10:01 +0200

    add header

---
+++ internal/wrappers/features/step_defs_cpp/steps.hpp
@@ -0,0 +1,42 @@
+#ifndef _STEPS_HPP_
+#define _STEPS_HPP_
+
+#include <boost/test/unit_test.hpp>
+#define EXPECT_EQ BOOST_CHECK_EQUAL
+#include <cucumber-cpp/autodetect.hpp>
+#include <iostream>
+
+#include "auth.hpp"
+#include "collection.hpp"
+#include "document.hpp"
+#include "index.hpp"
+#include "realtime.hpp"
+#include "kuzzle.hpp"
+
+#include "kuzzle_utils.h"
+
+#include "json_spirit/json_spirit.h"
+
+using cucumber::ScenarioScope;
+
+using namespace kuzzleio;
+using std::cout;
+using std::endl;
+using std::string;
+
+struct KuzzleCtx {
+  Kuzzle* kuzzle = NULL;
+  options kuzzle_options;
+
+  string user_id;
+  string index;
+  string collection;
+  string jwt;
+
+  user*                   currentUser        = NULL;
+  json_spirit::Value_type customUserDataType = json_spirit::null_type;
+
+  bool success;
+};
+
+#endif
