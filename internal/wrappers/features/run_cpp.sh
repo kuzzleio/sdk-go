fcf00385733776a633752e2ddb0b49b16a657d22
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2018-07-19 16:59:48 +0200

    Apply @scottinet requested changes

--- internal/wrappers/features/run_cpp.sh
+++ internal/wrappers/features/run_cpp.sh
@@ -6,8 +6,10 @@ sh ./build_cpp_tests.sh
 FEATURE_FILE=$1
 
 if [ -z "$KUZZLE_HOST" ]; then
+  # For debug
   ./_build_cpp_tests/KuzzleSDKStepDefs &
 else
+  # To hide cucumber debug output in CI
   ./_build_cpp_tests/KuzzleSDKStepDefs > /dev/null &
 fi
 
